91e6c433be362c9a155ea9cb991c88d96f9e6f17
mgwaiyanlin <waiyanlinn.netuser@gmail.com>
2024-09-25 01:41:57 +0630

    first commit

---
+++ GameShopWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameShopWebApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        
+    }
+}
---
+++ GameShopWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameShopWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
